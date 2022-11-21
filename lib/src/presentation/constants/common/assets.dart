562cbeb30815713c1cd305c29cbff09c58b9abb6
Mohsen <mniroomandj@gmail.com>
2022-11-21 11:05:08 +0330

    feat: add lottie animations

--- lib/src/presentation/constants/common/assets.dart
+++ lib/src/presentation/constants/common/assets.dart
@@ -1,3 +1,9 @@
 class Assets{
   static const String appLogo = "assets/svg/logo.svg";
+
+
+  /// Lottie
+  static const String loadingPrimary = "assets/lottie/loading_red.json";
+  static const String loadingSecondary = "assets/lottie/loading_white.json";
+
 }
