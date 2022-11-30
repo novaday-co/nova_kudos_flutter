973bd295c9ca17363b4ba23fe26a6202001acb02
Mohsen <mniroomandj@gmail.com>
2022-11-30 17:28:17 +0330

    feat: add extension to achievement

--- lib/src/domain/model/achievement/achievement.dart
+++ lib/src/domain/model/achievement/achievement.dart
@@ -1,13 +1,32 @@
+import 'package:nova_kudos_flutter/src/presentation/constants/common/assets.dart';
+
 enum MedalEnum { gold, silver, bronze }
 
 class Achievement {
   final MedalEnum? medalEnum;
   final String? title;
+  final String? username;
   final DateTime? dateTime;
 
   Achievement({
     this.medalEnum,
     this.title,
     this.dateTime,
+    this.username,
   });
+}
+
+extension AchievementExtension on Achievement {
+  String get achievementAsset {
+    switch (medalEnum) {
+      case MedalEnum.gold:
+        return Assets.goldMedal;
+
+      case MedalEnum.silver:
+        return Assets.silverMedal;
+
+      default:
+        return Assets.bronzeMedal;
+    }
+  }
 }
