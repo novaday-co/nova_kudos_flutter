4302db1795e42d6024e05022044d31fd69735420
Mohsen <mniroomandj@gmail.com>
2022-11-30 17:34:07 +0330

    feat: create recent achievements widget in home

---
+++ lib/src/presentation/pages/home_page/widgets/recent_achievement_widget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:nova_kudos_flutter/src/domain/model/achievement/achievement.dart';
+import 'package:nova_kudos_flutter/src/presentation/config/routes.dart';
+import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/context_extensions.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/achievement_item_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/text_widget.dart';
+
+class RecentAchievementWidget extends StatelessWidget {
+  const RecentAchievementWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TextWidget.semiBold(
+                context.getStrings.recentAchievement,
+                context: context,
+                additionalStyle: const TextStyle(fontSize: 16),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, Routes.profile);
+              },
+              child: TextWidget.regular(
+                context.getStrings.myMedals,
+                context: context,
+                additionalStyle: const TextStyle(fontSize: 14),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        SizedBox(
+          height: 110,
+          child: ListView.separated(
+            itemBuilder: (context, index) => AchievementItemWidget(
+              achievement: Achievement(),
+            ),
+            separatorBuilder: (context, index) => const SizedBox(
+              width: 8,
+            ),
+            itemCount: 6,
+            scrollDirection: Axis.horizontal,
+          ),
+        ),
+      ],
+    );
+  }
+}
