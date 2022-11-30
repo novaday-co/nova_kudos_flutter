a419142eafdd278f5a22c7c36d17d190f184c0b5
Mohsen <mniroomandj@gmail.com>
2022-11-30 15:28:13 +0330

    feat: create settings page

---
+++ lib/src/presentation/pages/settings_page/settings_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/text_widget.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextWidget.bold("Settings page",context:context,),
+    );
+  }
+}
