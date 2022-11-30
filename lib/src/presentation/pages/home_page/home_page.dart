61ed3aa769e5ddd7a3ca53b1c07cc463737d3a64
Mohsen <mniroomandj@gmail.com>
2022-11-30 17:35:22 +0330

    fix: add balance & recent achievement widget to home

--- lib/src/presentation/pages/home_page/home_page.dart
+++ lib/src/presentation/pages/home_page/home_page.dart
@@ -1,13 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:nova_kudos_flutter/src/presentation/ui/widgets/text_widget.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/home_cubit/home_cubit.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/balance_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/home_header_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/recent_achievement_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/base_stateless_widget.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+// class HomePage extends BaseStatefulWidget {
+//   const HomePage({Key? key}) : super(key: key);
+//
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+//
+// class _HomePageState
+//     extends BaseStatefulWidgetState<HomePage, HomeCubit> {
+//
+//   @override
+//   Widget body(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Column(
+//         children: [
+//
+//         ],
+//       ),
+//     );
+//   }
+//
+// }
+
+class HomePage extends BaseStatelessWidget<HomeCubit> {
+  const HomePage({Key? key}) : super(key: key, includeHorizontalPadding: false);
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: TextWidget.bold("Home page",context:context,),
+  Widget body(BuildContext context) {
+    return Column(
+      children: [
+        const HomeHeaderWidget(),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: const [
+                SizedBox(
+                  height: 32,
+                ),
+                HomeBalanceWidget(),
+                SizedBox(
+                  height: 24,
+                ),
+                RecentAchievementWidget()
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
