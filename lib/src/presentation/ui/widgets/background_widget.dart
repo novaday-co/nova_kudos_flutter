21a73372db912d629df0b0726260c28413e90f89
Mohsen <mniroomandj@gmail.com>
2022-11-30 17:35:49 +0330

    feat: add width & height

--- lib/src/presentation/ui/widgets/background_widget.dart
+++ lib/src/presentation/ui/widgets/background_widget.dart
@@ -8,6 +8,9 @@ class BackgroundWidget extends StatelessWidget {
   final double? verticalPadding;
   final double? horizontalPadding;
   final double? borderRadius;
+  final double? height;
+  final double? width;
+  final Color? borderColor;
 
   const BackgroundWidget({
     Key? key,
@@ -18,29 +21,37 @@ class BackgroundWidget extends StatelessWidget {
     this.onTap,
     this.backgroundColor,
     this.borderRadius,
+    this.height,
+    this.width,
+    this.borderColor,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: backgroundColor ?? Theme.of(context).colorScheme.onBackground,
-      shape: RoundedRectangleBorder(
-        side: BorderSide(
+    return SizedBox(
+      height: height,
+      width: width,
+      child: Material(
+        color: backgroundColor ?? Theme.of(context).colorScheme.onBackground,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
             color: isBordered
-                ? Theme.of(context).colorScheme.outline
-                : Colors.transparent),
-        borderRadius: BorderRadius.circular(borderRadius ?? 0),
-      ),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(borderRadius ?? 0),
-        splashColor: Theme.of(context).colorScheme.primary.withOpacity(0.23),
-        onTap: onTap,
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: horizontalPadding ?? 0,
-            vertical: verticalPadding ?? 0,
+                ? borderColor ?? Theme.of(context).colorScheme.outline
+                : Colors.transparent,
           ),
-          child: child,
+          borderRadius: BorderRadius.circular(borderRadius ?? 0),
+        ),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(borderRadius ?? 0),
+          splashColor: Theme.of(context).colorScheme.primary.withOpacity(0.23),
+          onTap: onTap,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: horizontalPadding ?? 0,
+              vertical: verticalPadding ?? 0,
+            ),
+            child: child,
+          ),
         ),
       ),
     );
