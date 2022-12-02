b0dad51abe98dfcea2d5297b72844a7e4b447f1b
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-02 15:04:59 +0330

    feat: notification list widget

---
+++ lib/src/presentation/pages/notification/widgets/notification_list_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/notification_cubit/notification_cubit.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/notification_cubit/notification_state.dart';
+import 'package:nova_kudos_flutter/src/domain/model/notification/notification_model.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/notification/widgets/notification_item_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/notification/widgets/notification_page_skeleton.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/background_widget.dart';
+
+class NotificationListWidget extends StatefulWidget {
+  const NotificationListWidget({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationListWidget> createState() => _NotificationListWidgetState();
+}
+
+class _NotificationListWidgetState extends State<NotificationListWidget> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundWidget(
+      borderRadius: 16,
+      backgroundColor: Colors.white,
+      child: BlocBuilder<NotificationCubit, NotificationState>(
+        builder: (context, state) {
+          if (state is NotificationRequestState) {
+            return state.when(
+              loading: () => Container(),
+              success: (notifications) => ListView.builder(
+                shrinkWrap: true,
+                itemCount: 15,
+                itemBuilder: (context, index) {
+                  return NotificationItemWidget(
+                    notificationModel: notifications[index],
+                  );
+                },
+              ),
+              failed: (error) => const SizedBox.shrink(),
+            );
+          }
+          return NotificationPageSkeleton();
+          return const SizedBox.shrink();
+        },
+      ),
+    );
+  }
+}
