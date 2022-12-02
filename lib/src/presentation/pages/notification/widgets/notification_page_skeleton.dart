05df16aec0c0d9fc14e69b72a627b2ad885ba71e
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-02 15:04:09 +0330

    feat: crate notification skeleton

---
+++ lib/src/presentation/pages/notification/widgets/notification_page_skeleton.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/background_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/skeleton_widget.dart';
+import 'package:skeleton_text/skeleton_text.dart';
+
+class NotificationPageSkeleton extends StatelessWidget {
+  const NotificationPageSkeleton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BackgroundWidget(
+      backgroundColor: Colors.white,
+      borderRadius: 16,
+      child: ListView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemCount: 15,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: const [
+                SkeletonWidget.circular(width: 32, height: 32),
+                SizedBox(width: 8),
+                SkeletonWidget.rectangular(width: 193, height: 14),
+                SizedBox(width: 8),
+                SkeletonWidget.rectangular(width: 35, height: 10),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
