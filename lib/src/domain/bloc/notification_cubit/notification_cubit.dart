e1681cb2caf2c1de3b20fcb9081030ef9c6d6359
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-02 15:03:45 +0330

    feat: mock notification

--- lib/src/domain/bloc/notification_cubit/notification_cubit.dart
+++ lib/src/domain/bloc/notification_cubit/notification_cubit.dart
@@ -5,5 +5,36 @@ import 'package:nova_kudos_flutter/src/domain/model/notification/notification_mo
 class NotificationCubit extends Cubit<NotificationState> {
   NotificationCubit() : super(InitNotificationState());
 
+  void getNotification() async {
+    List<NotificationModel> notifications = [
+      NotificationModel(
+          text: "دریافت سکه از طرف محسن نیرومند",
+          diffTime: "2روز پیش",
+          userProfile: "https://picsum.photos/seed/picsum/200/300"),
+      NotificationModel(
+          text: "دریافت سکه از طرف امیرمحمد جباری",
+          diffTime: "3روز پیش",
+          userProfile: "https://picsum.photos/seed/picsum/200/300"),
+      NotificationModel(
+          text: "دریافت سکه از طرف محسن نیرومند",
+          diffTime: "2روز پیش",
+          userProfile: "https://picsum.photos/seed/picsum/200/300"),
+      NotificationModel(
+          text: "دریافت سکه از طرف آرام امینی",
+          diffTime: "3روز پیش",
+          userProfile: "https://picsum.photos/seed/picsum/200/300"),
+      NotificationModel(
+          text: "دریافت سکه از طرف آرمان محمدی",
+          diffTime: "2روز پیش",
+          userProfile: "https://picsum.photos/seed/picsum/200/300"),
+      NotificationModel(
+          text: "دریافت سکه از طرف پریسا باستانی",
+          diffTime: "2روز پیش",
+          userProfile: "https://picsum.photos/seed/picsum/200/300"),
+    ];
+    emit(const NotificationRequestState.loading());
+    await Future.delayed(const Duration(seconds: 3));
+    emit(NotificationRequestState.success(notifications));
+  }
 
 }
