f601513b80180b64508bc31d0b4131c93ebbfcf5
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-03 11:47:37 +0330

    fix: resolve conflict

--- lib/src/presentation/config/route_generator.dart
+++ lib/src/presentation/config/route_generator.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/complete_profile_cubit/complete_profile_cubit.dart';
-import 'package:nova_kudos_flutter/src/domain/bloc/home_cubit/home_cubit.dart';
-import 'package:nova_kudos_flutter/src/domain/bloc/landing_cubit/landing_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/login_cubit/login_cubit.dart';
-import 'package:nova_kudos_flutter/src/domain/bloc/profile/profile_cubit.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/notification_cubit/notification_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/splash_cubit/splash_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/veirfy_code_cubit/verify_code_cubit.dart';
 import 'package:nova_kudos_flutter/src/presentation/config/routes.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/complete_profile/complete_profile_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/landing_page/landing_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/login_signup/login_page.dart';
-import 'package:nova_kudos_flutter/src/presentation/pages/profile/profile_page.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/notification/notification_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/splash/splash_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/verify_code/verify_code_page.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/profile/profile_page.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/profile/profile_cubit.dart';
 
 class RouteGenerator {
   static Map<String, WidgetBuilder> getRoutes(RouteSettings settings) {
@@ -45,6 +45,10 @@ class RouteGenerator {
         create: (context) => ProfileCubit(),
         child: const ProfilePage(),
       ),
+      Routes.notification: (context) => BlocProvider(
+        create: (context) => NotificationCubit(),
+        child: const NotificationPage(),
+      )
     };
   }
 }
