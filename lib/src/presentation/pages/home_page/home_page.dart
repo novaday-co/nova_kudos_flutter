ffbc33249e0dd2823470930ba260ec393eedbd60
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-03 10:58:22 +0330

    feat: complete home_page ui

--- lib/src/presentation/pages/home_page/home_page.dart
+++ lib/src/presentation/pages/home_page/home_page.dart
@@ -1,54 +1,58 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/home_cubit/home_cubit.dart';
-import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/balance_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/components/row_balance_component.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/home_header_widget.dart';
-import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/recent_achievement_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/achievements_listview_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/pulls_listview_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/home_page/widgets/voluntary_events_listview.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/widgets/base_stateless_widget.dart';
 
-// class HomePage extends BaseStatefulWidget {
-//   const HomePage({Key? key}) : super(key: key);
-//
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-//
-// class _HomePageState
-//     extends BaseStatefulWidgetState<HomePage, HomeCubit> {
-//
-//   @override
-//   Widget body(BuildContext context) {
-//     return SingleChildScrollView(
-//       child: Column(
-//         children: [
-//
-//         ],
-//       ),
-//     );
-//   }
-//
-// }
-
 class HomePage extends BaseStatelessWidget<HomeCubit> {
-  const HomePage({Key? key}) : super(key: key, includeHorizontalPadding: false);
+  const HomePage({Key? key})
+      : super(
+          key: key,
+          includeHorizontalPadding: false,
+          includeVerticalPadding: false,
+        );
 
   @override
   Widget body(BuildContext context) {
     return Column(
       children: [
-        const HomeHeaderWidget(),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+          child: HomeHeaderWidget(),
+        ),
         Expanded(
           child: SingleChildScrollView(
             child: Column(
               children: const [
                 SizedBox(
-                  height: 32,
+                  height: 24,
                 ),
-                HomeBalanceWidget(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: RowBalanceComponent(),
+                ),
                 SizedBox(
                   height: 24,
                 ),
-                RecentAchievementWidget()
+                RecentAchievementWidget(),
+                SizedBox(
+                  height: 20,
+                ),
+                HomePullsListViewWidget(),
+                SizedBox(
+                  height: 20,
+                ),
+                HomeVoluntaryEventsListview(),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  height: 74,
+                ),
               ],
             ),
           ),
