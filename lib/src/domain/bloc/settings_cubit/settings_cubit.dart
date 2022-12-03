25e2358030b09f2147237eaa34c345076da68c3b
Mohsen <mniroomandj@gmail.com>
2022-12-03 15:37:11 +0330

    feat: create logout function

--- lib/src/domain/bloc/settings_cubit/settings_cubit.dart
+++ lib/src/domain/bloc/settings_cubit/settings_cubit.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/settings_cubit/settings_state.dart';
+import 'package:nova_kudos_flutter/src/presentation/config/routes.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
   SettingsCubit() : super(InitSettingsState());
+
+  Future<void> logout(BuildContext context) async {
+    Future.delayed(const Duration(seconds: 3));
+    Navigator.pushNamedAndRemoveUntil(context, Routes.splash, (route) => false);
+  }
 }
