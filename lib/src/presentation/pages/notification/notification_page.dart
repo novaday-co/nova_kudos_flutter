e9b5c9116da25c1599642abad6f275ac9d9a95ac
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-03 11:48:30 +0330

    fix: resolve conflict

--- lib/src/presentation/pages/notification/notification_page.dart
+++ lib/src/presentation/pages/notification/notification_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/context_extensions.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/notification/widgets/notification_list_widget.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/widgets/app_bar_widget.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/widgets/base_stateless_widget.dart';
@@ -8,14 +9,12 @@ class NotificationPage extends BaseStatelessWidget {
 
   @override
   CustomAppbar? appBar(BuildContext context) {
-    return const CustomAppbar(
-      title: 'اطلاعیه ها',
+    return CustomAppbar(
+      title: context.getStrings.notifications,
       hasBackButton: true,
     );
   }
 
   @override
-  Widget body(BuildContext context) {
-    return const NotificationListWidget();
-  }
+  Widget body(BuildContext context) => const NotificationListWidget();
 }
