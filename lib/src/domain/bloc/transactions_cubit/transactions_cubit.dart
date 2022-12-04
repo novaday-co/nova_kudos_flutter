05b4800ce4dc6e571263ef49429c04c574de1419
Mohsen <mniroomandj@gmail.com>
2022-12-04 17:44:52 +0330

    feat: add get transaction function

--- lib/src/domain/bloc/transactions_cubit/transactions_cubit.dart
+++ lib/src/domain/bloc/transactions_cubit/transactions_cubit.dart
@@ -1,7 +1,59 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/transactions_cubit/transactions_state.dart';
+import 'package:nova_kudos_flutter/src/domain/model/transaction_model/transaction_model.dart';
 
 class TransactionsCubit extends Cubit<TransactionsState> {
 
   TransactionsCubit() : super(TransactionsInitState());
+
+  Future<void> getTransactions() async {
+    emit(const GetTransactionsState.loading());
+    await Future.delayed(const Duration(seconds: 2));
+    List<TransactionModel> list = [
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: true,
+      ),
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: true,
+      ),
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: true,
+      ),
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: false,
+      ),
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: false,
+      ),
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: true,
+      ),
+      TransactionModel(
+        title: "انتقال ۵۰ سکه به شهریار بیات",
+        cause: "بابت کارت تشکر",
+        createdAt: DateTime.now(),
+        isSuccessful: false,
+      ),
+    ];
+    emit( GetTransactionsState.success(list));
+
+  }
 }
