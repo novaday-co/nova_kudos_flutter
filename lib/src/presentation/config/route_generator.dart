6837005dfc7ff4c09343ffe5d74e3ce72a78fa01
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-04 15:22:24 +0330

    feat: add create shop route

--- lib/src/presentation/config/route_generator.dart
+++ lib/src/presentation/config/route_generator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/complete_profile_cubit/complete_profile_cubit.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/create_shop_cubit/create_shop_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/home_cubit/home_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/landing_cubit/landing_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/login_cubit/login_cubit.dart';
@@ -12,6 +13,7 @@ import 'package:nova_kudos_flutter/src/domain/bloc/splash_cubit/splash_cubit.dar
 import 'package:nova_kudos_flutter/src/domain/bloc/veirfy_code_cubit/verify_code_cubit.dart';
 import 'package:nova_kudos_flutter/src/presentation/config/routes.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/complete_profile/complete_profile_page.dart';
+import 'package:nova_kudos_flutter/src/presentation/pages/create_shop_page/create_shop_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/landing_page/landing_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/login_signup/login_page.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/notification/notification_page.dart';
@@ -62,6 +64,10 @@ class RouteGenerator {
       Routes.notification: (context) => BlocProvider(
             create: (context) => NotificationCubit(),
             child: const NotificationPage(),
+          ),
+      Routes.createShopPage: (context) => BlocProvider(
+            create: (context) => CreateShopCubit(),
+            child: const CreateShopPage(),
           )
     };
   }
--- lib/src/presentation/config/routes.dart
+++ lib/src/presentation/config/routes.dart
@@ -8,6 +8,7 @@ abstract class Routes {
   static const notification = _Paths.notification;
   static const profile = _Paths.profile;
   static const landingPage = _Paths.landingPage;
+  static const createShopPage = _Paths.createShopPage;
 }
 
 abstract class _Paths {
@@ -18,4 +19,5 @@ abstract class _Paths {
   static const notification = '/notification';
   static const profile = '/profile';
   static const landingPage = '/landing_page';
+  static const createShopPage = '/create_shop';
 }
