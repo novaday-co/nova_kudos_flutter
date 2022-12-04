c4f0429e5785ae6dc8cdc2dac4b0d02648e57551
Mohsen <mniroomandj@gmail.com>
2022-12-04 15:32:08 +0330

    feat: create custom lottie widget

--- lib/src/presentation/ui/widgets/lottie_animations_widget.dart
+++ lib/src/presentation/ui/widgets/lottie_animations_widget.dart
@@ -17,11 +17,34 @@ class LottieAnimations {
         child: Transform.scale(
           scale: 2.5,
           child: Lottie.asset(
-            isPrimary ? Assets.loadingPrimary : Assets.loadingSecondary,
+            isPrimary ? Assets.lottieLoadingPrimary : Assets.lottieLoadingSecondary,
             repeat: true,
           ),
         ),
       ),
     );
   }
+
+  static Widget customLottie({
+    required String lottieAsset,
+    double? height,
+    double? width,
+    double? scale,
+    bool repeat = false,
+  }) {
+    return SizedBox(
+      height: height ?? 40,
+      width: width ?? 40,
+      child: Center(
+        child: Transform.scale(
+          scale: scale,
+          child: Lottie.asset(
+            lottieAsset,
+            repeat: repeat,
+          ),
+        ),
+      ),
+    );
+  }
+
 }
