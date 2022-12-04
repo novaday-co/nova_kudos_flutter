e1556644d69173ae16063f0328fe3a7585bc44db
Mohsen <mniroomandj@gmail.com>
2022-12-04 17:43:33 +0330

    feat: add jalaliFormattedDate With time

--- lib/src/presentation/helpers/extensions/datetime_extension.dart
+++ lib/src/presentation/helpers/extensions/datetime_extension.dart
@@ -1,8 +1,14 @@
+import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/string_extension.dart';
 import 'package:shamsi_date/shamsi_date.dart';
 
 extension DateTimeExtension on DateTime {
   String get formattedJalaliDate {
     Jalali jalaliDate = toJalali();
     return '${jalaliDate.year}/${jalaliDate.month}/${jalaliDate.day}';
+  }
+
+  String get formattedJalaliDateWithTime {
+    Jalali jalaliDate = toJalali();
+    return '${jalaliDate.hour.toString().appendZeroPrefix}:${jalaliDate.minute.toString().appendZeroPrefix} - ${jalaliDate.year}/${jalaliDate.month}/${jalaliDate.day}';
   }
 }
