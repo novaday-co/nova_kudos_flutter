376daba54018ab0699994c9881f11592a61688e3
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-04 10:13:20 +0330

    feat: shop page skeleton

---
+++ lib/src/presentation/pages/shop_page/widgets/shop_page_skeleton.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/skeleton_widget.dart';
+
+class ShopPageSkeleton extends StatelessWidget {
+  const ShopPageSkeleton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        mainAxisExtent: 155,
+      ),
+      itemBuilder: (context, index) {
+        return const SkeletonWidget.rectangular(width: 160, height: 155);
+      },
+    );
+  }
+}
