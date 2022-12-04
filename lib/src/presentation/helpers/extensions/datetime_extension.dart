3c7f815d62f4eaf14d43aa6d0f8b7c2017d7f12a
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-04 17:20:50 +0330

    feat: difference datetime extension

--- lib/src/presentation/helpers/extensions/datetime_extension.dart
+++ lib/src/presentation/helpers/extensions/datetime_extension.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/context_extensions.dart';
 import 'package:shamsi_date/shamsi_date.dart';
+import 'package:sprintf/sprintf.dart';
 
 extension DateTimeExtension on DateTime {
   String get formattedJalaliDate {
     Jalali jalaliDate = toJalali();
     return '${jalaliDate.year}/${jalaliDate.month}/${jalaliDate.day}';
+  }
+
+  String differenceFromNow(BuildContext context) {
+    int inDaysDifference = difference(DateTime.now()).inDays;
+    return sprintf(context.getStrings.phDay, [inDaysDifference]);
   }
 }
