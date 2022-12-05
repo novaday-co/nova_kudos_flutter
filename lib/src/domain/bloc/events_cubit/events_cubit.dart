4cdf7742c773dd0c63c1cc6e7a0ebac1592ae562
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-05 15:36:35 +0330

    feat: events state

--- lib/src/domain/bloc/events_cubit/events_cubit.dart
+++ lib/src/domain/bloc/events_cubit/events_cubit.dart
@@ -3,4 +3,10 @@ import 'package:nova_kudos_flutter/src/domain/bloc/events_cubit/events_state.dar
 
 class EventsCubit extends Cubit<EventsState> {
   EventsCubit() : super(InitEventsState());
+
+  void getEvents() async {
+    emit(const GetEventsRequestState.loading());
+    await Future.delayed(const Duration(seconds: 3));
+    emit(const GetEventsRequestState.success());
+  }
 }
--- lib/src/domain/bloc/events_cubit/events_state.dart
+++ lib/src/domain/bloc/events_cubit/events_state.dart
@@ -1,3 +1,17 @@
-abstract class EventsState{}
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class InitEventsState extends EventsState{}
+part 'events_state.freezed.dart';
+
+abstract class EventsState {}
+
+class InitEventsState extends EventsState {}
+
+@freezed
+class GetEventsRequestState extends EventsState with _$GetEventsRequestState {
+  const factory GetEventsRequestState.loading() = LoadingGetEventsRequestState;
+
+  const factory GetEventsRequestState.success() = SuccessGetEventsRequestState;
+
+  const factory GetEventsRequestState.failed(String? error) =
+      FailedGetEventsRequestState;
+}
