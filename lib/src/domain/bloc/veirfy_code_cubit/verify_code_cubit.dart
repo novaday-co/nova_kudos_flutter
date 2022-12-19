bff8b2e421c086db08532034321c8c799ae5e376
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-19 12:09:52 +0330

    feat: call api verify

--- lib/src/domain/bloc/veirfy_code_cubit/verify_code_cubit.dart
+++ lib/src/domain/bloc/veirfy_code_cubit/verify_code_cubit.dart
@@ -1,10 +1,15 @@
 import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kiwi/kiwi.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/base_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/veirfy_code_cubit/verify_code_state.dart';
+import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/auth_repository/auth_repository.dart';
 
-class VerifyCodeCubit extends Cubit<VerifyCodeState> {
+class VerifyCodeCubit extends BaseCubit<VerifyCodeState> {
   late Timer timer;
+  AuthRepository authRepository = KiwiContainer().resolve<AuthRepository>();
 
   VerifyCodeCubit() : super(InitVerifyCodeState()) {
     initialTimer();
@@ -38,8 +43,17 @@ class VerifyCodeCubit extends Cubit<VerifyCodeState> {
 
   void postVerify({required String phoneNumber, required String otp}) async {
     emit(const LoadingVerifyRequestState());
-    await Future.delayed(const Duration(seconds: 2));
-    emit(const SuccessVerifyRequestState());
+    await safeCall(
+      apiCall: authRepository.verifyOtp(mobileNumber: phoneNumber, otp: otp),
+      onData: (resultStatus, resultModel) {
+        if (resultStatus == ResultStatus.success) {
+          emit(const VerifyRequestState.success());
+        }
+      },
+      onError: (error) {
+        emit(VerifyRequestState.failed(error));
+      },
+    );
   }
 
   void postResendCode() async {
