3bd76e9fa5df2bb360e40adff6ea9233692368b9
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-19 11:20:17 +0330

    feat: inject repository and api modules

--- lib/src/data/di/repository_module.dart
+++ lib/src/data/di/repository_module.dart
@@ -1,4 +1,7 @@
 import 'package:kiwi/kiwi.dart';
+import 'package:nova_kudos_flutter/src/data/api/auth/auth_api_impl.dart';
+import 'package:nova_kudos_flutter/src/data/repository/auth_repository/auth_repository.dart';
+import 'package:nova_kudos_flutter/src/data/repository/auth_repository/auth_repository_impl.dart';
 import 'package:nova_kudos_flutter/src/data/repository/local_repository/local_storage_repository.dart';
 import 'package:nova_kudos_flutter/src/data/repository/local_repository/local_storage_repository_impl.dart';
 
@@ -7,5 +10,10 @@ class RepositoryModule {
     KiwiContainer().registerFactory<LocalStorageRepository>((container) =>
         LocalStorageRepositoryImpl(
             keeper: container.resolve(), hive: container.resolve()));
+
+    KiwiContainer().registerFactory<AuthRepository>(
+      (container) =>
+          AuthRepositoryImpl(authApi: KiwiContainer().resolve()),
+    );
   }
 }
