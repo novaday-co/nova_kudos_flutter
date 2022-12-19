05bae159de2ddcd36b17d124fc488d5e1d275988
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-19 13:06:57 +0330

    feat: user model

--- lib/src/domain/model/user/user_model.dart
+++ lib/src/domain/model/user/user_model.dart
@@ -1,9 +1,13 @@
 class UserModel {
   String? name;
   String? image;
+  String? mobile;
+  String? token;
 
   UserModel({
     this.name,
     this.image,
+    this.mobile,
+    this.token
   });
 }
