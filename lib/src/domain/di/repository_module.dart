734440d7d47ab129db93987cb557f4bd0ad16ce1
Amir Jabbari <am.jabbarii@gmail.com>
2022-12-19 13:45:36 +0330

    feat: inject local storage repository

--- lib/src/domain/di/repository_module.dart
+++ lib/src/domain/di/repository_module.dart
@@ -4,16 +4,16 @@ import 'package:nova_kudos_flutter/src/domain/repository/auth_repository/auth_re
 import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository.dart';
 import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository_impl.dart';
 
-
 class RepositoryModule {
   static inject() {
     KiwiContainer().registerFactory<LocalStorageRepository>((container) =>
         LocalStorageRepositoryImpl(
             keeper: container.resolve(), hive: container.resolve()));
 
-    KiwiContainer().registerFactory<AuthRepository>(
-      (container) =>
-          AuthRepositoryImpl(authApi: KiwiContainer().resolve()),
-    );
+    KiwiContainer().registerFactory<AuthRepository>((container) =>
+        AuthRepositoryImpl(
+            authApi: container.resolve(),
+            localStorageRepository:
+                container.resolve<LocalStorageRepository>()));
   }
 }
