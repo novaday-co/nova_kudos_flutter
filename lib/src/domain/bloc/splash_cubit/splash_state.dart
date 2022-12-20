452712981db363ad94fd76d3193c762ac18f9fa4
Mohsen <mniroomandj@gmail.com>
2022-12-20 12:28:21 +0330

    feat: add check token states

--- lib/src/domain/bloc/splash_cubit/splash_state.dart
+++ lib/src/domain/bloc/splash_cubit/splash_state.dart
@@ -1,5 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'splash_state.freezed.dart';
+
 abstract class SplashState{}
 
-class SplashInitState extends SplashState{
+class SplashInitState extends SplashState{}
+
+@freezed
+class CheckUserSplashState extends SplashState with _$CheckUserSplashState {
+  const factory CheckUserSplashState.unAuthenticated() = UnAuthenticatedUserSplashState;
+
+  const factory CheckUserSplashState.authenticated() =
+  AuthenticatedUserSplashState;
 
 }
