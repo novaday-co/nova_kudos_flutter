9b59c01fec25ff6d6121c912b40dd1e18bad63d6
Mohsen <mniroomandj@gmail.com>
2022-12-20 12:28:12 +0330

    feat: add check token function

--- lib/src/domain/bloc/splash_cubit/splash_cubit.dart
+++ lib/src/domain/bloc/splash_cubit/splash_cubit.dart
@@ -1,13 +1,28 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/splash_cubit/splash_state.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashCubit extends Cubit<SplashState> {
+  LocalStorageRepository localStorageRepository;
 
-  SplashCubit() : super(SplashInitState());
+  SplashCubit({
+    required this.localStorageRepository,
+  }) : super(SplashInitState());
 
   Future<String> getAppVersion() async {
     final packageInfo = await PackageInfo.fromPlatform();
     return packageInfo.version;
+  }
+
+  void checkUserToken() async {
+    final token = await localStorageRepository.getToken();
+    await Future.delayed(const Duration(seconds: 2));
+    if(token != null){
+      emit(const CheckUserSplashState.authenticated());
+    }
+    else{
+      emit(const CheckUserSplashState.unAuthenticated());
+    }
   }
 }
