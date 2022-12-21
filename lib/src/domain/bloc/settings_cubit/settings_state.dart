c6cb1c8330ac59ab6d0f6ba58dafe178ffd5ae42
Mohsen <mniroomandj@gmail.com>
2022-12-21 12:39:16 +0330

    feat: add logout states

--- lib/src/domain/bloc/settings_cubit/settings_state.dart
+++ lib/src/domain/bloc/settings_cubit/settings_state.dart
@@ -1,9 +1,17 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:nova_kudos_flutter/src/domain/model/achievement/achievement.dart';
-
-// part 'settings_state.freezed.dart';
+part 'settings_state.freezed.dart';
 
 abstract class SettingsState {}
 
 class InitSettingsState extends SettingsState {}
 
+@freezed
+class LogoutSettingsState extends SettingsState with _$LogoutSettingsState {
+  const factory LogoutSettingsState.loading() =
+  LoadingLogoutSettingsState;
+
+  const factory LogoutSettingsState.success() =
+  SuccessLogoutSettingsState;
+
+}
