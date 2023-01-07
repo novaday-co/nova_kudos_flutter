b38d9ef5483fc8473e2b4457641725f8bf331b71
Mohsen Jalali <mniroomandj@gmail.com>
2023-01-07 21:07:19 +0330

    feat: extends shop cubit from pagination cubit

--- lib/src/domain/bloc/shop_cubit/shop_cubit.dart
+++ lib/src/domain/bloc/shop_cubit/shop_cubit.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/general/pagination_cubit/pagination_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/shop_cubit/shop_state.dart';
+import 'package:nova_kudos_flutter/src/domain/model/general/pagination_resource_model.dart';
 import 'package:nova_kudos_flutter/src/domain/model/shop/shop.dart';
 
-class ShopCubit extends Cubit<ShopState> {
-  ShopCubit() : super(InitShopState());
-
-  void getShop() async {
+class ShopCubit extends PaginationCubit<ShopModel> {
+  @override
+  Future<void> get({
+    RequestType requestType = RequestType.init,
+    int pageNumber = 1,
+    int pageSize = 20,
+  }) async {
     List<ShopModel> shopItems = [
       ShopModel(
         title: 'اعتبار فیدیبو',
@@ -56,8 +61,13 @@ class ShopCubit extends Cubit<ShopState> {
         price: 2,
       ),
     ];
-    emit(const GetShopRequestState.loading());
-    await Future.delayed(const Duration(seconds: 3));
-    emit(GetShopRequestState.success(shopItems));
+    emitLoading(requestType);
+    await Future.delayed(const Duration(seconds: 2));
+    emitLoaded(
+        PaginationResourceModel(
+          data: shopItems,
+          meta: MetaModel(lastPage: 1, currentPage: 1),
+        ),
+        requestType);
   }
 }
