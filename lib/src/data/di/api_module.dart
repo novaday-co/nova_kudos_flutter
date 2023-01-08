94aae6628ee889fc02b22a778e756baa640b2b62
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-08 14:58:17 +0330

    feat: inject api service

--- lib/src/data/di/api_module.dart
+++ lib/src/data/di/api_module.dart
@@ -3,11 +3,15 @@ import 'package:nova_kudos_flutter/src/data/api/auth/auth_api.dart';
 import 'package:nova_kudos_flutter/src/data/api/auth/auth_api_impl.dart';
 import 'package:nova_kudos_flutter/src/data/api/company/company_api.dart';
 import 'package:nova_kudos_flutter/src/data/api/company/company_api_impl.dart';
+import 'package:nova_kudos_flutter/src/data/api/user/user_api.dart';
+import 'package:nova_kudos_flutter/src/data/api/user/user_api_impl.dart';
 
 class ApiModule {
   static Future<void> inject() async {
     KiwiContainer().registerFactory<AuthApi>((container) => AuthApiImpl());
     KiwiContainer()
         .registerFactory<CompanyApi>((container) => CompanyApiImpl());
+    KiwiContainer()
+        .registerFactory<UserApi>((container) => UserApiImpl());
   }
 }
