feb7d6cae799015a742109dbfdfa86c05ae50543
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-08 10:41:30 +0330

    feat:  add coin system cubit and states

--- lib/src/domain/bloc/coin_rate_cubit/coin_rate_cubit.dart
+++ lib/src/domain/bloc/coin_rate_cubit/coin_rate_cubit.dart
@@ -1,6 +1,35 @@
+import 'package:kiwi/kiwi.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/base_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/coin_rate_cubit/coin_rate_state.dart';
+import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/company_repository/company_repository.dart';
 
 class CoinRateCubit extends BaseCubit<CoinRateState> {
+  CompanyRepository companyRepository =
+      KiwiContainer().resolve<CompanyRepository>();
+
   CoinRateCubit() : super(CoinRateStateInitState());
+
+  void validateCoinRate(String? input) {
+    if (input != null && input.isNotEmpty) {
+      emit(const CoinRateValidationState.valid());
+    } else {
+      emit(const CoinRateValidationState.invalid());
+    }
+  }
+
+  void getCoinRate() async {
+    emit(const CoinRateGetRequestState.loading());
+    await safeCall(
+      apiCall: companyRepository.getCompanyCoinValue(companyId: 1),
+      onData: (resultStatus, resultModel) {
+        if (resultStatus == ResultStatus.success) {
+          emit(CoinRateGetRequestState.success(resultModel!.data!));
+        }
+      },
+      onError: (error) {
+        emit(CoinRateGetRequestState.failed(error));
+      },
+    );
+  }
 }
--- lib/src/domain/bloc/coin_rate_cubit/coin_rate_state.dart
+++ lib/src/domain/bloc/coin_rate_cubit/coin_rate_state.dart
@@ -1,7 +1,27 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:nova_kudos_flutter/src/domain/model/company/coin_system_model.dart';
 
-//part 'coin_rate_state.freezed.dart';
+part 'coin_rate_state.freezed.dart';
 
 abstract class CoinRateState {}
 
 class CoinRateStateInitState extends CoinRateState {}
+
+@freezed
+class CoinRateValidationState extends CoinRateState {
+  const factory CoinRateValidationState.valid() = CoinRateValidState;
+
+  const factory CoinRateValidationState.invalid() = CoinRateInValidState;
+}
+
+@freezed
+class CoinRateGetRequestState extends CoinRateValidationState {
+  const factory CoinRateGetRequestState.loading() =
+      LoadingCoinRateGetRequestState;
+
+  const factory CoinRateGetRequestState.success(
+      CompanyCoinSystemModel coinSystemModel) = SuccessCoinRateGetRequestState;
+
+  const factory CoinRateGetRequestState.failed(String? error) =
+      FailedCoinRateGetRequestState;
+}
