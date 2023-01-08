9aac98a800536861ca18b0547f115b98dac2aaea
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-08 10:40:45 +0330

    feat: create company repository

---
+++ lib/src/domain/repository/company_repository/company_repository.dart
@@ -0,0 +1,8 @@
+import 'package:nova_kudos_flutter/src/domain/model/company/coin_system_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
+
+abstract class CompanyRepository {
+  Future<ResultModel<CompanyCoinSystemModel>> getCompanyCoinValue({
+    required int companyId,
+  });
+}
