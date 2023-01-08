15d4796baed0aa799f52c017a1c3a41075062b94
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-08 14:58:28 +0330

    feat: account detail model

---
+++ lib/src/domain/model/user_company/account_detail_model.dart
@@ -0,0 +1,9 @@
+import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
+
+class AccountDetailModel {
+  UserCompanyModel? userDefaultCompanyModel;
+
+  AccountDetailModel({
+    this.userDefaultCompanyModel,
+  });
+}
