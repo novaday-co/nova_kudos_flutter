7090271780f057f598c8532451e64b95b65d900a
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-08 14:59:20 +0330

    fix: call get profile in splash

--- lib/src/domain/bloc/splash_cubit/splash_cubit.dart
+++ lib/src/domain/bloc/splash_cubit/splash_cubit.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kiwi/kiwi.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/general/base_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/splash_cubit/splash_state.dart';
 import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/user_repository/user_repository.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
-class SplashCubit extends Cubit<SplashState> {
+class SplashCubit extends BaseCubit<SplashState> {
   LocalStorageRepository localStorageRepository;
+  final userRepo = KiwiContainer().resolve<UserRepository>();
 
   SplashCubit({
     required this.localStorageRepository,
@@ -18,10 +22,16 @@ class SplashCubit extends Cubit<SplashState> {
   void checkUserToken() async {
     final token = await localStorageRepository.getToken();
     await Future.delayed(const Duration(seconds: 2));
-    if(token != null){
+    if (token != null) {
+       await safeCall(
+        apiCall: userRepo.getProfile(),
+        onData: (resultStatus, resultModel) {
+
+        },
+      );
+
       emit(const CheckUserSplashState.authenticated());
-    }
-    else{
+    } else {
       emit(const CheckUserSplashState.unAuthenticated());
     }
   }
