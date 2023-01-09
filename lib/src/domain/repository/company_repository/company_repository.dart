feea9ca553f04466d0b4f6f05b3275fdd0b28b23
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-09 18:22:40 +0330

    feat: get product company repository

--- lib/src/domain/repository/company_repository/company_repository.dart
+++ lib/src/domain/repository/company_repository/company_repository.dart
@@ -1,4 +1,6 @@
 import 'package:nova_kudos_flutter/src/domain/model/company/coin/coin_system_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/company/product/product_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/general/pagination_resource_model.dart';
 import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
 
 abstract class CompanyRepository {
@@ -6,8 +8,12 @@ abstract class CompanyRepository {
     required int companyId,
   });
 
-  Future<ResultModel<CompanyCoinSystemModel>> setCompanyCoinValue({
+  Future<ResultModel<CompanyCoinSystemModel>> setCompanyCoinValue(
+      {required int companyId, required int coinValue});
+
+  Future<ResultModel<PaginationResourceModel<ProductModel>>> getProducts({
     required int companyId,
-    required int coinValue
+    required int pageSize,
+    required int pageIndex,
   });
 }
