c121fe57d16680a261452d02144c7364ee75f13f
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-09 18:20:13 +0330

    feat: add product model

---
+++ lib/src/domain/model/company/product/product_model.dart
@@ -0,0 +1,17 @@
+class ProductModel {
+  String? name;
+  int? currency;
+  int? coin;
+  int? amount;
+  String? avatar;
+  DateTime? expirationDate;
+
+  ProductModel({
+    this.name,
+    this.currency,
+    this.coin,
+    this.amount,
+    this.avatar,
+    this.expirationDate,
+  });
+}
