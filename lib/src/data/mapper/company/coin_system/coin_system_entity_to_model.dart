0eb151d026306af9468b839e5be5eddefaa65c0f
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-09 16:52:04 +0330

    fix: move coin entity

--- lib/src/data/mapper/company/coin_system/coin_system_entity_to_model.dart
+++ lib/src/data/mapper/company/coin_system/coin_system_entity_to_model.dart
@@ -1,4 +1,4 @@
-import 'package:nova_kudos_flutter/src/data/entity/company/coin_system_entity.dart';
+import 'package:nova_kudos_flutter/src/data/entity/company/coin/coin_system_entity.dart';
 import 'package:nova_kudos_flutter/src/domain/model/company/coin/coin_system_model.dart';
 
 extension CoinSystemMapper on CoinSystemEntity {
