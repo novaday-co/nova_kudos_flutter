029a0afb7c8c4aeaffabe95def877d4477a58497
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-10 11:47:43 +0330

    feat: add default company in base cubit

--- lib/src/domain/bloc/general/base_cubit.dart
+++ lib/src/domain/bloc/general/base_cubit.dart
@@ -1,33 +1,41 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kiwi/kiwi.dart';
 import 'package:nova_kudos_flutter/src/data/utils/exception.dart';
 import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository.dart';
 
 class BaseCubit<T> extends Cubit<T> {
   BaseCubit(T initialState) : super(initialState);
 
+  LocalStorageRepository localStorageRepository =
+      KiwiContainer().resolve<LocalStorageRepository>();
+
   Future<void> safeCall<D>({
     required Future<ResultModel<D>> apiCall,
-    Function(FailedResultStatus?,String?)? onError,
-    Function( ResultModel<D>?)? onData,
+    Function(FailedResultStatus?, String?)? onFailed,
+    Function(ResultModel<D>?)? onSuccess,
   }) async {
     try {
       final response = await apiCall;
       if (response.isFailure) {
-        onError?.call(response.failedStatus,response.error);
-      }
-      else{
-        onData?.call(response);
+        onFailed?.call(response.failedStatus, response.error);
+      } else {
+        onSuccess?.call(response);
       }
     } on DioError catch (error) {
       if (error.error is KodusException) {
         addError(error.error);
-        onError?.call(null,error.error.toString());
+        onFailed?.call(null, error.error.toString());
       } else {
-        onError?.call(null,error.message);
+        onFailed?.call(null, error.message);
       }
     }
   }
+
+  Future<UserCompanyModel> get defaultCompany async =>
+      await localStorageRepository.getUser();
 
   @override
   void emit(T state) {
