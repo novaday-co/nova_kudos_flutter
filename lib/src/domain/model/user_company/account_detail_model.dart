66d9af7bead311a7adf0c1f34ac52542eb430a79
Mohsen <mniroomandj@gmail.com>
2023-01-10 17:32:24 +0330

    fix: remove

--- lib/src/domain/model/user_company/account_detail_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
-
-class AccountDetailModel {
-  UserCompanyModel? userDefaultCompanyModel;
-
-  AccountDetailModel({
-    this.userDefaultCompanyModel,
-  });
-}
