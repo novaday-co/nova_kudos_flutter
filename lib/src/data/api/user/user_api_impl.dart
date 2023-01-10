6d1f70040fc36c83456d7b343eed2a5891387126
Mohsen <mniroomandj@gmail.com>
2023-01-10 17:36:03 +0330

    fix: change get profile generic type into user company entity

--- lib/src/data/api/user/user_api_impl.dart
+++ lib/src/data/api/user/user_api_impl.dart
@@ -1,16 +1,16 @@
 import 'package:nova_kudos_flutter/src/data/api/user/user_api.dart';
 import 'package:nova_kudos_flutter/src/data/entity/api_response.dart';
-import 'package:nova_kudos_flutter/src/data/entity/user_company/user_company_collection.dart';
+import 'package:nova_kudos_flutter/src/data/entity/user_company/user_company_entity.dart';
 
 class UserApiImpl extends UserApi {
   @override
-  Future<ApiResponse<UserCompanyCollection>> getProfile() async {
+  Future<ApiResponse<UserCompanyEntity>> getProfile() async {
     final response = await apiService.get(
       'users/account-detail',
     );
     return ApiResponse.fromResponse(
       response: response,
-      resultMapper: (data) => UserCompanyCollection.fromJson(data),
+      resultMapper: (data) => UserCompanyEntity.fromJson(data),
     );
   }
 }
