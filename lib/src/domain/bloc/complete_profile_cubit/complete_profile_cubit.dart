725a5fb05b73379fc11cc0f6f469fa26447008bf
Mohsen <mniroomandj@gmail.com>
2023-01-10 17:32:56 +0330

    feat: extends from file cubit

--- lib/src/domain/bloc/complete_profile_cubit/complete_profile_cubit.dart
+++ lib/src/domain/bloc/complete_profile_cubit/complete_profile_cubit.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/complete_profile_cubit/complete_profile_state.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_cubit.dart';
+import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/bottom_sheets/media_picker_sheet.dart';
 
-class CompleteProfileCubit extends Cubit<CompleteProfileState> {
-  CompleteProfileCubit() : super(CompleteProfileInitState());
+class CompleteProfileCubit extends FileCubit {
+  LocalStorageRepository localStorageRepository;
+
+  CompleteProfileCubit({
+    required this.localStorageRepository,
+  }) : super(CompleteProfileInitState()){
+   getUserCompanyModelFromPref();
+  }
+
+  UserCompanyModel? userCompanyModel;
+
+  void getUserCompanyModelFromPref() async {
+    emit(const CompleteProfileGetUserState.loading());
+    userCompanyModel = await localStorageRepository.getUser();
+    print(userCompanyModel?.avatar);
+    userCompanyModel?.phoneNumber = "0902432424";
+    emit(const CompleteProfileGetUserState.success());
+  }
 
   void profileForm(String? input) {
     if (input != null && input.isNotEmpty) {
@@ -16,7 +34,7 @@ class CompleteProfileCubit extends Cubit<CompleteProfileState> {
 
   void selectProfilePicture(BuildContext context) async {
     final imagePath = await showAccessMediaPickerSheet(context);
-    if(imagePath != null){
+    if (imagePath != null) {
       emit(CompleteProfilePictureState.selected(imagePath));
     }
   }
