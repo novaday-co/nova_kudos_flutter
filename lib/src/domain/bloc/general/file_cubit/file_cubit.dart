8d46c1e27c6fba18aeca8bcdce8e9d9d78925f4f
Mohsen <mniroomandj@gmail.com>
2023-01-11 16:24:24 +0330

    feat: add emits methods

--- lib/src/domain/bloc/general/file_cubit/file_cubit.dart
+++ lib/src/domain/bloc/general/file_cubit/file_cubit.dart
@@ -1,62 +1,71 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:kiwi/kiwi.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/general/base_cubit.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_state.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/file_repository/file_repository.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/bottom_sheets/media_picker_sheet.dart';
 
-abstract class FileCubit extends BaseCubit<BaseFileState> {
+abstract class FileCubit<S extends BaseFileState>
+    extends BaseCubit<BaseFileState> {
+  FileRepository fileRepository = KiwiContainer().resolve<FileRepository>();
+
   FileCubit(super.initialState);
 
-  StreamController streamController = StreamController<int>();
+  StreamController<double> progressStream = StreamController<double>();
 
-  String parseFileName(File file) => file.path.split('/').last;
-
-  Future<FormData> getFormData(Map<String, dynamic> data) async {
-    Map<String, dynamic> fromData = {};
-    fromData.forEach((key, value) async {
-      if (value is File) {
-        data.addEntries([
-          MapEntry(
-              key,
-              await MultipartFile.fromFile(value.path,
-                  filename: parseFileName(value)))
-        ]);
-      }
-    });
-    return FormData.fromMap(fromData);
+  String parseFileName(File file) {
+    return file.path.split('/').last;
   }
 
-  Future<void> upload() async {}
+  void onSendProgress(int count, int total) {
+    progressStream.sink.add(count / total);
+  }
 
-  // Future<void> uploadFile(
-  //     {required String url, CancelToken? cancelToken, data}) async {
-  //   await safeCall(
-  //     apiCall: fileRepository.upload(
-  //       url: url,
-  //       bodyParameters: data,
-  //       cancelToken: cancelToken,
-  //       onSendProgress: (count, total) {
-  //         // print(((count / total)) * 100);
-  //         emit(UploadFileState.loading((count / total) * 100));
-  //       },
-  //     ),
-  //     onData: (result) {
-  //       print("======================onData=========================");
-  //       emit(const UploadFileState.success());
-  //     },
-  //     onError: (failedStatus, error) {
-  //       print("======================error=========================");
-  //       emit(UploadFileState.failed(error));
-  //     },
-  //   );
-  // }
+  void selectImage(BuildContext context) async {
+    if (state is! UploadingFileState) {
+      final imagePath = await showAccessMediaPickerSheet(context);
+      if (imagePath != null) {
+        emit(SelectImageFileState.select(File(imagePath)));
+      }
+    }
+  }
+
+  Future<void> upload(File file) async {}
 
   Future<void> download() async {}
 
+  emitDownloading() {
+    progressStream = StreamController<double>();
+    emit(const DownloadFileState.loading(0));
+  }
+
+  emitUploading() {
+    progressStream = StreamController<double>();
+    emit(const UploadFileState.loading(0));
+  }
+
+  emitSuccessDownloading() {
+    emit(const DownloadFileState.success());
+  }
+
+  emitSuccessUploading() {
+    emit(const UploadFileState.success());
+  }
+
+  emitFailedDownloading(String? error) {
+    emit(DownloadFileState.failed(error));
+  }
+
+  emitFailedUploading(String? error) {
+    emit(UploadFileState.failed(error));
+  }
+
   @override
   Future<void> close() {
-    streamController.close();
+    progressStream.close();
     return super.close();
   }
 }
