8a6a3cc1aeb7f159c2c0763d37f09e3680a41825
Mohsen <mniroomandj@gmail.com>
2023-01-11 16:24:55 +0330

    feat: add download states

--- lib/src/domain/bloc/general/file_cubit/file_state.dart
+++ lib/src/domain/bloc/general/file_cubit/file_state.dart
@@ -1,15 +1,30 @@
+import 'dart:io';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:nova_kudos_flutter/src/domain/bloc/general/pagination_cubit/pagination_cubit.dart';
 part 'file_state.freezed.dart';
 
 abstract class BaseFileState {}
 
 @freezed
-class UploadFileState extends BaseFileState {
+class UploadFileState extends BaseFileState with _$UploadFileState {
   const factory UploadFileState.loading(double progress) = UploadingFileState;
 
   const factory UploadFileState.success() = SuccessUploadingFileState;
 
   const factory UploadFileState.failed(String? error) =
-  FailedUploadingRequestState;
+  FailedUploadingFileState;
+}
+
+@freezed
+class DownloadFileState extends BaseFileState with _$DownloadFileState {
+  const factory DownloadFileState.loading(double progress) = DownloadingFileState;
+
+  const factory DownloadFileState.success() = SuccessDownloadingFileState;
+
+  const factory DownloadFileState.failed(String? error) =
+  FailedDownloadingFileState;
+}
+
+@freezed
+class SelectImageFileState extends BaseFileState with _$SelectImageFileState {
+  const factory SelectImageFileState.select(File imageFile) = SelectedImageFileState;
 }
