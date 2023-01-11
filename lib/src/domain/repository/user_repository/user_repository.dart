af52441cd9b4cd5ec25f4feb4795f57e533b1c49
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-11 16:16:57 +0330

    feat: create purchase product in user repository

--- lib/src/domain/repository/user_repository/user_repository.dart
+++ lib/src/domain/repository/user_repository/user_repository.dart
@@ -1,8 +1,12 @@
+import 'package:nova_kudos_flutter/src/domain/model/purchase/purchase_model.dart';
 import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
-import 'package:nova_kudos_flutter/src/domain/model/user_company/account_detail_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
 
 abstract class UserRepository{
 
-  Future<ResultModel<AccountDetailModel>> getProfile();
+  Future<ResultModel<UserCompanyModel>> getProfile();
+
+  Future<ResultModel<PurchaseModel>> purchaseProduct(int productId);
+
 
 }
--- lib/src/domain/repository/user_repository/user_repository_impl.dart
+++ lib/src/domain/repository/user_repository/user_repository_impl.dart
@@ -1,8 +1,11 @@
 import 'package:nova_kudos_flutter/src/data/api/user/user_api.dart';
 import 'package:nova_kudos_flutter/src/data/mapper/api_response_to_result_model.dart';
+import 'package:nova_kudos_flutter/src/data/mapper/purchase/pourchase_entity_to_model.dart';
 import 'package:nova_kudos_flutter/src/data/mapper/user_company/user_company_entity_to_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/purchase/purchase_model.dart';
 import 'package:nova_kudos_flutter/src/domain/model/result_model.dart';
 import 'package:nova_kudos_flutter/src/domain/model/user_company/account_detail_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
 import 'package:nova_kudos_flutter/src/domain/repository/local_repository/local_storage_repository.dart';
 import 'package:nova_kudos_flutter/src/domain/repository/user_repository/user_repository.dart';
 
@@ -14,12 +17,22 @@ class UserRepositoryImpl extends UserRepository {
       {required this.userApi, required this.localStorageRepository});
 
   @override
-  Future<ResultModel<AccountDetailModel>> getProfile() async {
+  Future<ResultModel<UserCompanyModel>> getProfile() async {
     final response = await userApi.getProfile();
     if (response.isSuccess) {
-      localStorageRepository
-          .setUserCompany(response.data?.defaultUserCompany!.mapToModel);
+      localStorageRepository.setUserCompany(response.data!.mapToModel);
     }
+    return ApiToResultMapper.mapTo(
+      response: response,
+      dataMapper: () => response.data!.mapToModel,
+    );
+  }
+
+  @override
+  Future<ResultModel<PurchaseModel>> purchaseProduct(
+    int productId,
+  ) async {
+    final response = await userApi.purchaseProduct(productId);
     return ApiToResultMapper.mapTo(
       response: response,
       dataMapper: () => response.data!.mapToModel,
