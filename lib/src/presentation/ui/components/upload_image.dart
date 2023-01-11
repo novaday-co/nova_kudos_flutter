30b8380841fc62bb386513212b75c8fe5f6f3ea7
Mohsen <mniroomandj@gmail.com>
2023-01-11 16:26:55 +0330

    feat: add progress widget

--- lib/src/presentation/ui/components/upload_image.dart
+++ lib/src/presentation/ui/components/upload_image.dart
@@ -1,177 +1,163 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:kiwi/kiwi.dart';
-import 'package:nova_kudos_flutter/src/domain/bloc/file_cubit/file_cubit.dart';
-import 'package:nova_kudos_flutter/src/domain/bloc/file_cubit/file_state.dart';
-import 'package:nova_kudos_flutter/src/presentation/constants/common/assets.dart';
-import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/context_extensions.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_cubit.dart';
+import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_state.dart';
 import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/dart_extension.dart';
-import 'package:nova_kudos_flutter/src/presentation/ui/bottom_sheets/media_picker_sheet.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/widgets/background_widget.dart';
-import 'package:nova_kudos_flutter/src/presentation/ui/widgets/icon_widget.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/widgets/image_widget.dart';
-import 'package:nova_kudos_flutter/src/presentation/ui/widgets/tag_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/loading_widget.dart';
+import 'package:nova_kudos_flutter/src/presentation/ui/widgets/text_widget.dart';
 
-enum UploadImageViewType { circular, rectangular }
-
-class UploadImage extends StatefulWidget {
+class UploadImage<C extends FileCubit> extends StatefulWidget {
   final String? image;
   final String uploadImageUrl;
-  final UploadImageViewType imageViewType;
+  final Widget? tagWidget;
+  final double? width;
+  final double height;
+  final BoxShape? shape;
+  final Color? backgroundColor;
+  final Alignment? tagAlignment;
+  final Widget? nullImageWidget;
 
   const UploadImage({
     Key? key,
     this.image,
     required this.uploadImageUrl,
-    this.imageViewType = UploadImageViewType.rectangular,
+    this.tagWidget,
+    this.width,
+    required this.height,
+    this.shape,
+    this.tagAlignment,
+    this.nullImageWidget,
+    this.backgroundColor,
   }) : super(key: key);
 
   @override
-  State<UploadImage> createState() => _UploadImageState();
+  State<UploadImage> createState() => _UploadImageState<C>();
 }
 
-class _UploadImageState extends State<UploadImage> {
+class _UploadImageState<C extends FileCubit> extends State<UploadImage> {
   String? image;
-  late FileCubit fileCubit;
+
+  C get cubit => context.read<C>();
 
   @override
   void initState() {
     image = widget.image;
-    fileCubit = FileCubit(fileRepository: KiwiContainer().resolve());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return isCircular
-        ? imageView
-        : BlocProvider(
-            create: (_) => fileCubit,
-            child: BackgroundWidget(
-              height: 80,
-              width: context.screenWidth,
-              borderRadius: 16,
-              onTap: () {
-                selectPicture();
-              },
-              child: image != null
-                  ? Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        imageView,
-                        BlocBuilder<FileCubit, BaseFileState>(
-                          builder: (context, state) {
-                            return AnimatedContainer(
-                              duration: const Duration(milliseconds: 300),
-                              width: context.screenWidth,
-                              height: 80 *
-                                  (100 -
-                                      (state
-                                              .isA<UploadingFileState>()
-                                              ?.progress ??
-                                          0)) /
-                                  100,
-                              color: Colors.black.withOpacity(0.1),
-                            );
-                          },
-                        )
-                      ],
-                    )
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconWidget(
-                          icon: Assets.iconGallery,
-                          size: 24,
-                          height: 24,
-                          width: 24,
-                          iconColor:
-                              Theme.of(context).colorScheme.tertiaryContainer,
-                        ),
-                        const SizedBox(height: 4),
-                        TagWidget.rectangle(
-                          backgroundColor:
-                              Theme.of(context).colorScheme.onPrimary,
-                          value: context.getStrings.eventPicture,
-                          padding: 4,
-                          textStyle: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w700,
-                          ),
-                        )
-                      ],
-                    ),
-            ),
-          );
-  }
-
-  ImageType get imageType {
-    if (image?.startsWith("http") ?? false) {
-      return ImageType.network;
-    }
-    return ImageType.file;
-  }
-
-  bool get isCircular {
-    return widget.imageViewType == UploadImageViewType.circular;
-  }
-
-  Widget get imageView {
-    return ImageLoaderWidget(
-      imageUrl: image ?? "",
-      width: isCircular ? 90 : context.screenWidth,
-      height: 90,
-      imageType: imageType,
-      boxShape: isCircular ? BoxShape.circle : BoxShape.rectangle,
-      hasTag: true,
-      tagAlignment: isCircular ? Alignment.bottomCenter : Alignment.bottomRight,
-      onTap: () {
-        selectPicture();
-      },
-      tagWidget: isCircular
-          ? Align(
-              heightFactor: 0.0,
-              child: TagWidget.rectangle(
-                padding: 4,
-                value: context.getStrings.changeProfilePicture,
-                backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                textStyle: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w700,
+    return BlocListener<C, BaseFileState>(
+      listenWhen: _listenWhenUploadState,
+      listener: _listenToFileCubit,
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          BackgroundWidget(
+            height: widget.height,
+            width: widget.width,
+            borderRadius: 16,
+            shape: widget.shape,
+            clipBehaviour: Clip.antiAlias,
+            backgroundColor: widget.backgroundColor ?? Colors.transparent,
+            onTap: () => cubit.selectImage(context),
+            child: Stack(
+              children: [
+                imageView,
+                BlocBuilder<C, BaseFileState>(
+                  buildWhen: _buildWhenUploadingProgress,
+                  builder: (context, state) {
+                    return AnimatedCrossFade(
+                      duration: const Duration(milliseconds: 600),
+                      crossFadeState: state is UploadingFileState ? CrossFadeState
+                          .showSecond : CrossFadeState.showFirst,
+                      firstChild: const SizedBox(),
+                      secondChild: StreamBuilder<double>(
+                        initialData: 0,
+                        stream: cubit.progressStream.stream,
+                        builder: (context, progress) {
+                          return Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              AnimatedContainer(
+                                duration: const Duration(milliseconds: 300),
+                                height: (progress.data ?? 0) * widget.height,
+                                alignment: Alignment.bottomCenter,
+                                width: widget.width,
+                                decoration: BoxDecoration(
+                                  color: Theme
+                                      .of(context)
+                                      .colorScheme
+                                      .onSurface
+                                      .withOpacity(0.5),
+                                  borderRadius:
+                                  (widget.shape == BoxShape.circle)
+                                      ? null
+                                      : BorderRadius.circular(16),
+                                ),
+                              ),
+                              Center(
+                                child: Visibility(
+                                  visible: progress.data != 1,
+                                  replacement: const Loading(),
+                                  child: TextWidget.medium(
+                                    "${((progress.data ?? 0) * 100).toInt()}%",
+                                    context: context,
+                                    additionalStyle:
+                                    const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        },
+                      ),
+                    );
+                  },
                 ),
-              ),
-            )
-          : Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InkWell(
-                child: TagWidget.rectangle(
-                  backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                  value: context.getStrings.changePicture,
-                  padding: 4,
-                  textStyle: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 10,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ),
+              ],
             ),
+          ),
+          Align(
+            alignment: widget.tagAlignment ?? Alignment.bottomCenter,
+            child: widget.tagWidget,
+          )
+        ],
+      ),
     );
   }
 
-  void selectPicture() async {
-    final imagePath = await showAccessMediaPickerSheet(context);
-    if (imagePath != null) {
-      image = imagePath;
-      String? filename = image?.split('/').last;
-      setState(() {});
-      FormData body = FormData.fromMap({
-        "avatar": await MultipartFile.fromFile(imagePath, filename: filename)
-      });
-      fileCubit.uploadFile(url: widget.uploadImageUrl, data: body);
+  Widget get imageView {
+    if (widget.nullImageWidget != null && image == null) {
+      return widget.nullImageWidget!;
     }
+    return ImageLoaderWidget(
+      imageUrl: image ?? "",
+      width: widget.width,
+      height: widget.height,
+      boxShape: widget.shape ?? BoxShape.rectangle,
+    );
   }
+
+  bool _buildWhenUploadingProgress(BaseFileState previous,
+      BaseFileState current) {
+    return current is UploadFileState;
+  }
+
+  bool _listenWhenUploadState(BaseFileState previous, BaseFileState current) {
+    return current is UploadFileState || current is SelectImageFileState;
+  }
+
+  void _listenToFileCubit(BuildContext context, BaseFileState state) {
+    state.isA<SelectImageFileState>()?.when(
+      select: (imageFile) {
+        image = imageFile.path;
+        setState(() {});
+      },
+    );
+  }
+
+
 }
