78346f441d300859d5e5e50ebee6da65d0a0f0d1
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-11 16:17:04 +0330

    feat: create purchase product in user api

--- lib/src/data/api/user/user_api.dart
+++ lib/src/data/api/user/user_api.dart
@@ -1,9 +1,15 @@
 import 'package:nova_kudos_flutter/src/data/api/base_api.dart';
 import 'package:nova_kudos_flutter/src/data/entity/api_response.dart';
+import 'package:nova_kudos_flutter/src/data/entity/purchase/purchase_entity.dart';
 import 'package:nova_kudos_flutter/src/data/entity/user_company/user_company_collection.dart';
+import 'package:nova_kudos_flutter/src/data/entity/user_company/user_company_entity.dart';
 
 abstract class UserApi extends BaseApi{
 
-   Future<ApiResponse<UserCompanyCollection>> getProfile();
+   Future<ApiResponse<UserCompanyEntity>> getProfile();
+
+   Future<ApiResponse<PurchaseEntity>> purchaseProduct(int productId);
+
+
 
 }
