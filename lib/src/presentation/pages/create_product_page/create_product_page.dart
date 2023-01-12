d2fc939336511dee4fa537578688fb5769b078f7
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-12 18:01:29 +0330

    feat: ui create product page

--- lib/src/presentation/pages/create_product_page/create_product_page.dart
+++ lib/src/presentation/pages/create_product_page/create_product_page.dart
@@ -5,6 +5,7 @@ import 'package:nova_kudos_flutter/src/domain/bloc/create_shop_cubit/create_shop
 import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_state.dart';
 import 'package:nova_kudos_flutter/src/presentation/constants/common/assets.dart';
 import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/context_extensions.dart';
+import 'package:nova_kudos_flutter/src/presentation/helpers/extensions/dart_extension.dart';
 import 'package:nova_kudos_flutter/src/presentation/pages/create_product_page/params/create_product_page_params.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/components/upload_image.dart';
 import 'package:nova_kudos_flutter/src/presentation/ui/widgets/app_bar_widget.dart';
@@ -35,14 +36,20 @@ class CreateShopPage extends BaseStatelessWidget<CreateShopCubit> {
   @override
   Widget? bottomNavigation() {
     return BlocBuilder<CreateShopCubit, BaseFileState>(
+      buildWhen: _buildWhenCreateButton,
       builder: (context, state) => Padding(
         padding: const EdgeInsets.all(16.0),
         child: SafeArea(
           child: CustomButton.fill(
             context: context,
             text: context.getStrings.submit,
+            loadingStatus: _buttonLoadingStatus(state),
+            isEnable: state is CreateProductValidFormState ||
+                state is CreateProductState,
             loadingType: ButtonLoadingType.percentage,
-            onPressed: () {},
+            onPressed: () {
+              context.read<CreateShopCubit>().postNewProduct();
+            },
           ),
         ),
       ),
@@ -51,109 +58,147 @@ class CreateShopPage extends BaseStatelessWidget<CreateShopCubit> {
 
   @override
   Widget body(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          BlocBuilder<CreateShopCubit, BaseFileState>(
-            buildWhen: _buildWhenProfilePicture,
-            builder: (context, state) {
-              return UploadImage<CreateShopCubit>(
-                uploadImageUrl:"",
-                image: state is SelectedImageFileState
-                    ? state.imageFile.path
-                    : params?.imageUrl,
-                height: 90,
-                width: 1000,
-                tagAlignment: Alignment.bottomRight,
-                nullImageWidget: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconWidget(
-                        icon: Assets.iconGallery,
-                        size: 24,
-                        height: 24,
-                        width: 24,
-                        iconColor:
-                            Theme.of(context).colorScheme.tertiaryContainer,
-                      ),
-                      const SizedBox(height: 4),
-                      TagWidget.rectangle(
-                        backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                        value: context.getStrings.eventPicture,
+    return BlocListener<CreateShopCubit, BaseFileState>(
+      listener: _listenToCreateProductState,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            BlocBuilder<CreateShopCubit, BaseFileState>(
+              buildWhen: _buildWhenProfilePicture,
+              builder: (context, state) {
+                return UploadImage<CreateShopCubit>(
+                  uploadImageUrl: "",
+                  image: state is SelectedImageFileState
+                      ? state.imageFile.path
+                      : params?.imageUrl,
+                  height: 90,
+                  width: 1000,
+                  tagAlignment: Alignment.bottomRight,
+                  nullImageWidget: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconWidget(
+                          icon: Assets.iconGallery,
+                          size: 24,
+                          height: 24,
+                          width: 24,
+                          iconColor:
+                              Theme.of(context).colorScheme.tertiaryContainer,
+                        ),
+                        const SizedBox(height: 4),
+                        TagWidget.rectangle(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.onPrimary,
+                          value: context.getStrings.eventPicture,
+                          padding: 4,
+                          textStyle: TextStyle(
+                            color: Theme.of(context).primaryColor,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  tagWidget: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: InkWell(
+                      child: TagWidget.rectangle(
+                        backgroundColor:
+                            Theme.of(context).colorScheme.onPrimary,
+                        value: context.getStrings.changePicture,
                         padding: 4,
                         textStyle: TextStyle(
                           color: Theme.of(context).primaryColor,
                           fontSize: 10,
                           fontWeight: FontWeight.w700,
                         ),
-                      )
-                    ],
-                  ),
-                ),
-                tagWidget: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: InkWell(
-                    child: TagWidget.rectangle(
-                      backgroundColor: Theme.of(context).colorScheme.onPrimary,
-                      value: context.getStrings.changePicture,
-                      padding: 4,
-                      textStyle: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 10,
-                        fontWeight: FontWeight.w700,
                       ),
                     ),
                   ),
-                ),
-              );
-            },
-          ),
-          const SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            initValue: params?.shopName,
-            label: context.getStrings.shopName,
-            textInputType: TextInputType.text,
-            onChanged: (value) {},
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          CustomTextField(
-            initValue: params?.validity,
-            label: context.getStrings.validityPeriod,
-            textInputType: TextInputType.text,
-            onChanged: (value) {},
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          CustomTextField(
-            initValue: params?.count.toString(),
-            label: context.getStrings.numberOfInventory,
-            textInputType: TextInputType.number,
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          CustomTextField(
-            initValue: params?.coinCount.toString(),
-            label: context.getStrings.numberOfCoins,
-            textInputType: TextInputType.number,
-          ),
-        ],
+                );
+              },
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              initValue: params?.shopName,
+              label: context.getStrings.shopName,
+              textInputType: TextInputType.text,
+              onChanged: (value) {
+                context.read<CreateShopCubit>().setProductName(value);
+              },
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            CustomTextField(
+              initValue: params?.validity,
+              label: context.getStrings.validityPeriod,
+              textInputType: TextInputType.text,
+              onChanged: (value) {},
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            CustomTextField(
+              initValue: params?.count.toString(),
+              label: context.getStrings.numberOfInventory,
+              textInputType: TextInputType.number,
+              onChanged: (value) =>
+                  context.read<CreateShopCubit>().setProductAmount(value),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            CustomTextField(
+              initValue: params?.coinCount.toString(),
+              label: context.getStrings.productPrice,
+              textInputType: TextInputType.number,
+              onChanged: (value) {
+                context.read<CreateShopCubit>().setProductValue(value);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
 
   ///region Bloc When Conditions Functions
 
-  bool _buildWhenProfilePicture(
-      BaseFileState previous, BaseFileState current) {
+  bool _buildWhenProfilePicture(BaseFileState previous, BaseFileState current) {
     return current is SelectedImageFileState;
   }
 
+  bool _buildWhenCreateButton(BaseFileState previous, BaseFileState current) {
+    return current is CreateProductState ||
+        current is CreateProductFormValidationState;
+  }
+
   ///endregion
+  ///region Bloc Listeners
+  void _listenToCreateProductState(BuildContext context, BaseFileState state) {
+    state.isA<CreateProductState>()?.whenOrNull(
+          success: () {
+            context.showSuccessSnackBar(context.getStrings.productAddedSuccess);
+            Navigator.pop(context, true);
+          },
+          failed: (error) => context.showFailedSnackBar(error),
+        );
+  }
+
+  /// endregion
+
+  ButtonLoadingStatus _buttonLoadingStatus(BaseFileState state) {
+    if (state is LoadingCreateProductState) {
+      return ButtonLoadingStatus.loading;
+    }
+    if (state is SuccessCreateProductState) {
+      return ButtonLoadingStatus.complete;
+    }
+    return ButtonLoadingStatus.normal;
+  }
 }
