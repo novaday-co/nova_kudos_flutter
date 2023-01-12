404898274030b5c35738eea4539deaa954e77784
Amir Jabbari <am.jabbarii@gmail.com>
2023-01-12 18:01:59 +0330

    feat: api call and handle states new product

--- lib/src/domain/bloc/create_shop_cubit/create_shop_cubit.dart
+++ lib/src/domain/bloc/create_shop_cubit/create_shop_cubit.dart
@@ -1,13 +1,71 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/create_shop_cubit/create_shop_state.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_cubit.dart';
-import 'package:nova_kudos_flutter/src/presentation/ui/bottom_sheets/media_picker_sheet.dart';
+import 'package:nova_kudos_flutter/src/domain/model/company/product/product_model.dart';
+import 'package:nova_kudos_flutter/src/domain/model/user_company/user_company_model.dart';
+import 'package:nova_kudos_flutter/src/domain/repository/company_repository/company_repository.dart';
 
 class CreateShopCubit extends FileCubit<CreateShopState> {
-  CreateShopCubit() : super(InitCreateShopState());
+  CompanyRepository companyRepository;
+  ProductModel productModel = ProductModel();
 
+  CreateShopCubit({
+    required this.companyRepository,
+  }) : super(InitCreateShopState());
 
+  Future<void> postNewProduct() async {
+    emit(const CreateProductState.loading());
+    UserCompanyModel userCompanyModel = await defaultCompany;
+    productModel.avatar = imageFile?.path;
+    await safeCall(
+      apiCall: companyRepository.postNewProduct(
+        companyId: userCompanyModel.companyId ?? -1,
+        productModel: productModel,
+      ),
+      onSuccess: (result) {
+        emit(const CreateProductState.success());
+      },
+      onFailed: (failedStatus, error) {
+        emit(CreateProductState.failed(error));
+      },
+    );
+  }
 
+  void checkFormValidation() {
+    if (productModel.name != null &&
+        productModel.currency != null &&
+        productModel.amount != null) {
+      if (productModel.name!.isNotEmpty &&
+          productModel.currency!.toString().isNotEmpty&&
+          productModel.amount!.toString().isNotEmpty) {
+        emit(const CreateProductFormValidationState.valid());
+      }else{
+        emit(const CreateProductFormValidationState.invalid());
+      }
+    } else {
+      emit(const CreateProductFormValidationState.invalid());
+    }
+  }
 
+  void setProductName(String name) {
+    productModel.name = name;
+    checkFormValidation();
+  }
+
+  void setProductAmount(String amount) {
+    if(amount.toString().isNotEmpty){
+      productModel.amount = int.parse(amount);
+    }else{
+      productModel.amount=null;
+    }
+    checkFormValidation();
+  }
+
+  void setProductValue(String currency) {
+    if(currency.toString().isNotEmpty){
+      productModel.currency = int.parse(currency);
+    }else{
+      productModel.currency=null;
+    }
+    checkFormValidation();
+  }
 }
--- lib/src/domain/bloc/create_shop_cubit/create_shop_state.dart
+++ lib/src/domain/bloc/create_shop_cubit/create_shop_state.dart
@@ -1,15 +1,27 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:nova_kudos_flutter/src/domain/bloc/general/file_cubit/file_state.dart';
 
-// part 'create_shop_state.freezed.dart';
+part 'create_shop_state.freezed.dart';
 
 abstract class CreateShopState extends BaseFileState {}
 
 class InitCreateShopState extends CreateShopState {}
 
-// @freezed
-// class CreateShopPictureState extends CreateShopState
-//     with _$CreateShopPictureState {
-//   const factory CreateShopPictureState.selected(String imagePath) =
-//       SelectCreateShopPictureState;
-// }
+@freezed
+class CreateProductFormValidationState extends CreateShopState {
+  const factory CreateProductFormValidationState.valid() =
+      CreateProductValidFormState;
+
+  const factory CreateProductFormValidationState.invalid() =
+      CreateProductInValidFormState;
+}
+
+@freezed
+class CreateProductState extends CreateShopState with _$CreateProductState{
+  const factory CreateProductState.loading() = LoadingCreateProductState;
+
+  const factory CreateProductState.success() = SuccessCreateProductState;
+
+  const factory CreateProductState.failed(String? error) =
+      FailedCreateProductState;
+}
